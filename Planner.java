f80d50a3c6e5a55f9fae6f633c04132cf837e3ff
Novem <novemjanydzel@gmail.com>
2024-03-05 00:04:18 +0000

    Fixed capacity issue

--- Planner.java
+++ Planner.java
@@ -1,36 +1,42 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.List;
 
 public class Planner {
 
 
-    private static List<Place> selectPlaces (List<Place> places,int budget, int days) {
+    private static List<Place> selectPlaces(List<Place> places, int budget, int days) {
         int N = places.size();
-        int dp[][] = new int[N + 1][budget + 1];
+        int[][] dp = new int[N + 1][budget + 1];
 
-        for (Place place: places){
-            for(int d = days;d>0;d--){
-                for (int w = budget;w>=place.cost;w--){
-                    dp[d][w] = Math.max(dp[d][w],place.rating + dp[d-1][w-place.cost]);
+        // Dynamic programming table calculation
+        for (int i = 1; i <= N; i++) {
+            for (int w = 1; w <= budget; w++) {
+                if (places.get(i - 1).cost <= w) {
+                    dp[i][w] = Math.max(places.get(i - 1).rating + dp[i - 1][w - places.get(i - 1).cost], dp[i - 1][w]);
+                } else {
+                    dp[i][w] = dp[i - 1][w];
                 }
             }
         }
 
-        List<Place>selectedPlaces = new ArrayList<>();
-        int d = days;
-        int w = budget;
+        // Trace back to find which places to visit
+        List<Place> selectedPlaces = new ArrayList<>();
+        int remainingBudget = budget;
 
-        while (d>0 && w>0){
-            if(dp[d][w]!=dp[d-1][w]){
-                selectedPlaces.add(places.get(d-1));
-                w-=places.get(d-1).cost;
-                d--;
-            }
-            else{
-                d--;
+        for (int i = N; i > 0 && days > 0; i--) {
+            // Check if the place at index i-1 was included
+            if (dp[i][remainingBudget] != dp[i-1][remainingBudget]) {
+                selectedPlaces.add(places.get(i-1));
+                remainingBudget -= places.get(i-1).cost; // Decrease the budget
+                days--; // Decrease the days
             }
         }
+
+        // The selected places list will be in reverse order, reverse it if necessary
+        Collections.reverse(selectedPlaces);
+
         return selectedPlaces;
     }
 
@@ -39,11 +45,10 @@ public class Planner {
     private static void printItinerary(List<Place>selectedPlaces){
         System.out.println("Your Itinerary:");
         for (int i = 0; i< selectedPlaces.size();i++){
-            System.out.println("Day" + (i+1) + ": " + selectedPlaces.get(i).name + "Cost " + selectedPlaces.get(i).cost + ", Rating: " + selectedPlaces.get(i).rating + ")" );
+            System.out.println("Day" + (i+1) + ": " + selectedPlaces.get(i).name + " Cost " + selectedPlaces.get(i).cost + ", Rating: " + selectedPlaces.get(i).rating + ")" );
         }
 
     }
-
 
     public static void main (String args[]){
         Scanner scanner =  new Scanner(System.in);
@@ -62,11 +67,12 @@ public class Planner {
         places.add(new Place("Bacaui Park",350,80));
         places.add(new Place("Calle Crisologo",500,90));
         places.add(new Place("Polo Beach",480,75));
+        places.add(new Place("Eco Park", 310,90));
+        places.add(new Place("Banayoyo",200,75));
+        places.add(new Place("Baluarte",500,90));
+        places.add(new Place("Candon Park",560,85));
 
         List<Place> selectedPlaces = selectPlaces(places, budget, days);
         printItinerary(selectedPlaces);
     }
-
-
-
 }
